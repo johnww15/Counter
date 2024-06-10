e915a4976e61bff039a2a1c102484af49099b848
Jonathan Q <jonathan.quekww@gmail.com>
2024-06-10 22:13:00 +0800

    refactored test file to make it more readable

--- src/__tests__/Counter.test.js
+++ src/__tests__/Counter.test.js
@@ -11,40 +11,33 @@ describe("Counter Component", () => {
     cleanup();
   });
 
+  const getCountValue = () => screen.getByTestId("count").textContent;
+  const clickButton = (buttonText) =>
+    fireEvent.click(screen.getByText(buttonText));
+
   test("ensure initial count starts at 0", () => {
-    const countValue = screen.getByTestId("count").textContent;
-    expect(countValue).toBe("0");
+    expect(getCountValue()).toBe("0");
   });
 
   test("increases count by 1 when Increase button is clicked", () => {
-    const increaseButton = screen.getByText("Increase");
-    fireEvent.click(increaseButton);
-    const countValue = screen.getByTestId("count").textContent;
-    expect(countValue).toBe("1");
+    clickButton("Increase");
+    expect(getCountValue()).toBe("1");
   });
 
-  test("decreses count by 1 when Decrease button is clicked", () => {
-    const decreaseButton = screen.getByText("Decrease");
-    fireEvent.click(decreaseButton);
-    const countValue = screen.getByTestId("count").textContent;
-    expect(countValue).toBe("-1");
+  test("decreases count by 1 when Decrease button is clicked", () => {
+    clickButton("Decrease");
+    expect(getCountValue()).toBe("-1");
   });
 
   test("count is reset to 0 when Reset button is clicked", () => {
-    const resetButton = screen.getByText("Reset");
-    const decreaseButton = screen.getByText("Decrease");
-    fireEvent.click(decreaseButton);
-    fireEvent.click(resetButton);
-    const countValue = screen.getByTestId("count").textContent;
-    expect(countValue).toBe("0");
+    clickButton("Decrease");
+    clickButton("Reset");
+    expect(getCountValue()).toBe("0");
   });
 
   test("count is multiplied by -1 when -/+ button is clicked", () => {
-    const swapButton = screen.getByText("-/+");
-    const decreaseButton = screen.getByText("Decrease");
-    fireEvent.click(decreaseButton);
-    fireEvent.click(swapButton);
-    const countValue = screen.getByTestId("count").textContent;
-    expect(countValue).toBe("1");
+    clickButton("Decrease");
+    clickButton("-/+");
+    expect(getCountValue()).toBe("1");
   });
 });
