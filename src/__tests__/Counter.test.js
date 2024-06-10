42b6b81a5e0c33c366d477c61477f1374947c4fe
Jonathan Q <jonathan.quekww@gmail.com>
2024-06-10 22:12:29 +0800

    included swap button test

--- src/__tests__/Counter.test.js
+++ src/__tests__/Counter.test.js
@@ -29,4 +29,22 @@ describe("Counter Component", () => {
     const countValue = screen.getByTestId("count").textContent;
     expect(countValue).toBe("-1");
   });
+
+  test("count is reset to 0 when Reset button is clicked", () => {
+    const resetButton = screen.getByText("Reset");
+    const decreaseButton = screen.getByText("Decrease");
+    fireEvent.click(decreaseButton);
+    fireEvent.click(resetButton);
+    const countValue = screen.getByTestId("count").textContent;
+    expect(countValue).toBe("0");
+  });
+
+  test("count is multiplied by -1 when -/+ button is clicked", () => {
+    const swapButton = screen.getByText("-/+");
+    const decreaseButton = screen.getByText("Decrease");
+    fireEvent.click(decreaseButton);
+    fireEvent.click(swapButton);
+    const countValue = screen.getByTestId("count").textContent;
+    expect(countValue).toBe("1");
+  });
 });
