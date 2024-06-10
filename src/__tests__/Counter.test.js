105a04b48211f6e1d3ef995f1d6c6b15221bd7ad
Jonathan Q <jonathan.quekww@gmail.com>
2024-06-10 22:02:25 +0800

    included test to check for initial count in the component

--- src/__tests__/Counter.test.js
+++ src/__tests__/Counter.test.js
@@ -11,6 +11,11 @@ describe("Counter Component", () => {
     cleanup();
   });
 
+  test("ensure initial count starts at 0", () => {
+    const countValue = screen.getByTestId("count").textContent;
+    expect(countValue).toBe("0");
+  });
+
   test("increases count by 1 when Increase button is clicked", () => {
     const increaseButton = screen.getByText("Increase");
     fireEvent.click(increaseButton);
